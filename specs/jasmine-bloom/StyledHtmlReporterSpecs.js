4e2369c3f76a03e1d3698fb48912bbe1c580098c
Rudy Lattae <rudylattae@gmail.com>
2010-10-01 17:59:45 -0600

    Brought in the work done so far on the StyledReporter from the jasmine-aroma project.

--- specs/jasmine-bloom/StyledHtmlReporterSpecs.js
+++ specs/jasmine-bloom/StyledHtmlReporterSpecs.js
@@ -1,5 +1,51 @@
 describe('jasmine.bloom.StyledHtmlReporter', function() {
-    it('should be available', function() {
-        expect(jasmine.bloom.StyledHtmlReporter).not.toBe(null);
+    var env;
+    var reporter;
+    var body;
+    var fakeDocument;
+    var FeatureStory = jasmine.aroma.FeatureStory;
+    
+    // these helpers were ripped clean out of the jasmin's TrivialReporterSpec
+    beforeEach(function() {
+        env = new jasmine.Env();
+        env.updateInterval = 0;
+        jasmine.aroma._currentEnv = env;
+        
+        body = document.createElement("body");
+        fakeDocument = { body: body, location: { search: "" }};
+        reporter = new jasmine.aroma.StyledReporter(fakeDocument);
+        reporter = new jasmine.TrivialReporter(fakeDocument);
+    });
+
+    function findElements(divs, withClass) {
+        var els = [];
+        for (var i = 0; i < divs.length; i++) {
+            if (divs[i].className == withClass) els.push(divs[i]);
+        }
+        return els;
+    };
+
+    function findElement(divs, withClass) {
+        var els = findElements(divs, withClass);
+        if (els.length > 0) return els[0];
+            throw new Error("couldn't find div with class \"" + withClass + "\"");
+    }    
+    
+    it('should output the spec results to the document as div tags', function() {
+        var runner = env.currentRunner();
+        env.describe("A passing suite", function() {
+            env.it("here be passing specs", function() {
+                this.expect(true).toBeTruthy();
+            });
+        });
+        
+        env.addReporter(reporter);
+        runner.execute();
+                
+        var divs = fakeDocument.body.getElementsByTagName("div");
+        var passedSpecDiv = findElement(divs, 'suite passed');
+        
+        expect(passedSpecDiv.className).toEqual('suite passed');
+        expect(passedSpecDiv.innerHTML).toContain("here be passing specs");
     });
 });
--- src/jasmine-bloom/jasmine-bloom.js
+++ src/jasmine-bloom/jasmine-bloom.js
@@ -1,4 +1,28 @@
+/**
+ * Jasmine-Bloom - Cleaner reporting for your Jasmine specs
+ * Copyright (C) 2010, Rudy Lattae
+ * License: Simplified BSD
+ * 
+ * Jasmine-Bloom provides a slightly modified Html Reporter that outputs additional 
+ * meta-data relating to your specs. The aim is to make it easier to augment the 
+ * visual display of spec report.
+ * 
+ * Although jasmine-bloom is completely compatible with output from 
+ * the standard "Describe" and "it" grammar, I encourage you to try it out
+ * with jasmine-aroma's "feature", "story", etc. constructs. It's cool.
+ */
+ 
 // Top level namespace for the package
 jasmine.bloom = (typeof jasmine.bloom === 'undefined') ? {} : jasmine.bloom;
 
-jasmine.bloom.VERSION = '0.0.1dev';
+jasmine.bloom.VERSION = '0.1.0dev';
+
+
+jasmine.aroma.StyledReporter = function(doc) {
+  this.document = doc || document;
+  this.suiteDivs = {};
+  this.logRunningSpecs = false;
+};
+
+jasmine.aroma.StyledReporter.prototype = new jasmine.TrivialReporter();
+jasmine.aroma.StyledReporter.prototype.constructor = jasmine.aroma.StyledReporter;
