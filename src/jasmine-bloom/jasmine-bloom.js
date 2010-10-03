ec2b92bd45a431498678240d6f8d135d6e25cd6d
Rudy Lattae <rudylattae@gmail.com>
2010-10-03 00:14:42 -0600

    Upped the version

--- src/jasmine-bloom/jasmine-bloom.js
+++ src/jasmine-bloom/jasmine-bloom.js
@@ -15,7 +15,7 @@
 // Top level namespace for the package
 jasmine.bloom = (typeof jasmine.bloom === 'undefined') ? {} : jasmine.bloom;
 
-jasmine.bloom.VERSION = '0.2.5dev';
+jasmine.bloom.VERSION = '0.3.0dev';
 
 
 jasmine.bloom.StyledReporter = function(doc) {
