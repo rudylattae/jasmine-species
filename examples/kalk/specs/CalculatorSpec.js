02460f89938ea8ec010b57029a4163a168453b55
rudylattae <rudylattae@gmail.com>
2010-10-20 01:15:54 -0600

    Reordered the specs to read more clearly as a progrssion from finer details to higher level api stuff.

--- examples/kalk/specs/CalculatorSpec.js
+++ examples/kalk/specs/CalculatorSpec.js
@@ -1,15 +1,10 @@
-describe('kalk.Calculator', function() {    
-    xdescribe('add', function() {
-        it('should calculate the sum of the given operands', function() {
+describe('kalk.Calculator', function() {
+    describe('buffer', function() {
+        it('should initialize with 0 as the first entry', function() {
             var calc = new kalk.Calculator();
             
-            calc.input(5);
-            calc.input('+')
-            calc.input(4);
-            calc.input('=');
-            
-            expect(calc.result).toEqual(9);
-        });
+            expect(calc.buffer[0]).toBe(0);
+        });    
     });
     
     describe('input', function() {
@@ -17,21 +12,6 @@ describe('kalk.Calculator', function() {
         
         beforeEach(function() {
             calc = new kalk.Calculator();
-        });
-        
-        it('should overwrite the last buffer entry if it is number and the new input is also a number', function() {
-            expect(calc.buffer[0]).toBe(0);
-            
-            calc.input(5);
-            expect(calc.buffer[0]).toBe(5);
-            calc.input(14);
-            expect(calc.buffer[0]).toBe(14);
-        });
-        
-        it('should fail on any non-numeric and non-valid operation', function() {             
-            expect(function() {
-                 calc.input('r');
-            }).toThrow('Invalid input. "r" is not a number or a valid operation');
         });
         
         it('should accept regular numbers', function() {             
@@ -75,13 +55,33 @@ describe('kalk.Calculator', function() {
                  calc.input('=');
             }).not.toThrow();
         });
+        
+        it('should fail on any non-numeric and non-valid operation', function() {             
+            expect(function() {
+                 calc.input('r');
+            }).toThrow('Invalid input. "r" is not a number or a valid operation');
+        });
+        
+        it('should overwrite the last buffer entry if it is number and the new input is also a number', function() {
+            expect(calc.buffer[0]).toBe(0);
+            
+            calc.input(5);
+            expect(calc.buffer[0]).toBe(5);
+            calc.input(14);
+            expect(calc.buffer[0]).toBe(14);
+        });
     });
 
-    describe('buffer', function() {
-        it('should initialize with 0 as the first entry', function() {
+    xdescribe('add', function() {
+        it('should calculate the sum of the given operands', function() {
             var calc = new kalk.Calculator();
             
-            expect(calc.buffer[0]).toBe(0);
-        });    
+            calc.input(5);
+            calc.input('+')
+            calc.input(4);
+            calc.input('=');
+            
+            expect(calc.result).toEqual(9);
+        });
     });
 });
