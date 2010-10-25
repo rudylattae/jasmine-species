835b2b1146ee34929bb2adf0494f2e7bca9e23b9
rudylattae <rudylattae@gmail.com>
2010-10-24 19:25:40 -0600

    Cleaned up header doc for bloom.

--- src/jasmine-bloom/jasmine-bloom.js
+++ src/jasmine-bloom/jasmine-bloom.js
@@ -1,15 +1,11 @@
 /**
- * Jasmine-Bloom - Cleaner reporting for your Jasmine specs
+ * Jasmine-Bloom - Relaxed reporting for your Jasmine specs
  * Copyright (C) 2010, Rudy Lattae
  * License: Simplified BSD
  * 
  * Jasmine-Bloom provides a slightly modified Html Reporter that outputs additional 
  * meta-data relating to your specs. The aim is to make it easier to modify the 
  * presentation of the spec report.
- * 
- * Although jasmine-bloom is completely compatible with output from 
- * the standard "Describe" and "it" grammar, I encourage you to try it out
- * with jasmine-aroma's "feature", "story", etc. constructs. It's cool there too.
  */
  
 // Top level namespace for the package
