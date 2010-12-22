7bc08c8b28c85bda1c8b5d90b088d9ea06a47362
rudylattae <rudylattae@gmail.com>
2010-12-22 00:41:06 -0600

    Edited the GWT specs for clarity.

--- spec/jasmine-grammar/GWTSpecs.js
+++ spec/jasmine-grammar/GWTSpecs.js
@@ -16,13 +16,13 @@ describe('jasmine.grammar.GWT', function() {
     });
     
     describe('given', function() {
-        it('should create a runs block within the current spec', function() {
+        it('creates a runs block within the current spec', function() {
             GWT.given('the system is in a known state', function() { return "given"; });
             
             expect(parentSpec.queue.blocks[1].func()).toEqual("given");
         });
         
-        it('should add the block description prefixed with "Given" to the spec details', function() {
+        it('adds the block description prefixed with "Given" to the spec details', function() {
             GWT.given('the system is in a known state', function() { return "given"; });
             
             expect(parentSpec.details[0]).toEqual('Given the system is in a known state');
@@ -30,13 +30,13 @@ describe('jasmine.grammar.GWT', function() {
     });
     
     describe('when', function() {
-        it('should create a runs block within the current spec', function() {
+        it('creates a runs block within the current spec', function() {
             GWT.when('an event occurs', function() { return "when"; });
             
             expect(parentSpec.queue.blocks[1].func()).toEqual("when");
         });
         
-        it('should add the block description prefixed with "When" to the spec details', function() {
+        it('adds the block description prefixed with "When" to the spec details', function() {
             GWT.when('an event occurs', function() { return "when"; });
             
             expect(parentSpec.details[0]).toEqual('When an event occurs');
@@ -44,13 +44,13 @@ describe('jasmine.grammar.GWT', function() {
     });
     
     describe('then', function() {
-        it('should create a runs block within the current spec', function() {
+        it('creates a runs block within the current spec', function() {
             GWT.then('expect this outcome', function() { return "then"; });
             
             expect(parentSpec.queue.blocks[1].func()).toEqual("then");
         });
         
-        it('should add the block description prefixed with "Then" to the spec details', function() {
+        it('adds the block description prefixed with "Then" to the spec details', function() {
             GWT.then('expect this outcome', function() { return "then"; });
             
             expect(parentSpec.details[0]).toEqual('Then expect this outcome');
@@ -58,13 +58,13 @@ describe('jasmine.grammar.GWT', function() {
     });
     
     describe('and', function() {
-        it('should create a runs block within the current spec', function() {
+        it('creates a runs block within the current spec', function() {
             GWT.and('expect this outcome too', function() { return "and"; });
             
             expect(parentSpec.queue.blocks[1].func()).toEqual("and");
         });
         
-        it('should add the block description prefixed with "And" to the spec details', function() {
+        it('adds the block description prefixed with "And" to the spec details', function() {
             GWT.and('expect this outcome too', function() { return "and"; });
             
             expect(parentSpec.details[0]).toEqual('And expect this outcome too');
@@ -72,13 +72,13 @@ describe('jasmine.grammar.GWT', function() {
     });
     
     describe('but', function() {
-        it('should create a runs block within the current spec', function() {
+        it('creates a runs block within the current spec', function() {
             GWT.but('expect this outcome', function() { return "but"; });
             
             expect(parentSpec.queue.blocks[1].func()).toEqual("but");
         });
         
-        it('should add the block description prefixed with "But" to the spec details', function() {
+        it('adds the block description prefixed with "But" to the spec details', function() {
             GWT.but('expect this outcome', function() { return "but"; });
             
             expect(parentSpec.details[0]).toEqual('But expect this outcome');
@@ -86,7 +86,7 @@ describe('jasmine.grammar.GWT', function() {
     });
     
     describe('"given, when, then" when used as steps in a spec', function() {
-        it('should create runs blocks with matching details within the current spec for all steps', function() {
+        it('creates runs blocks in sequence with matching details within the current spec for all steps', function() {
             GWT.given('a piece of cake', function() { return "I am a given"; });
             GWT.when('I eat it', function() { return "I am a when"; });
             GWT.then('I should have it too', function() { return "I am a then"; });
