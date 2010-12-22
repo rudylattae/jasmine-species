8dbf46dc9680806fab5f52179d1da8302e209c8d
rudylattae <rudylattae@gmail.com>
2010-12-22 00:47:05 -0600

    Edited the FeatureStory specs for clarity.

--- spec/jasmine-grammar/FeatureStorySpecs.js
+++ spec/jasmine-grammar/FeatureStorySpecs.js
@@ -8,31 +8,34 @@ describe('jasmine.grammar.FeatureStory', function() {
     });
     
     describe('feature', function() {
-        it('should generate a suite with extended attributes', function() {
+        it('generates a suite marked as a "Feature"', function() {
             var suite = FeatureStory.feature('Some cool feature', function() {});
+            
             expect(suite.description).toBe('Feature: Some cool feature');
             expect(suite.tags).toEqual(['feature']);
         });
     });
     
     describe('story', function() {
-        it('should generate a suite with extended attributes', function() {
+        it('generates a suite marked as a "Story"', function() {
             var suite = FeatureStory.story('A user story', function() {});
+            
             expect(suite.description).toBe('Story: A user story');
             expect(suite.tags).toEqual(['story']);
         });
     });
     
     describe('component', function() {
-        it('should generate a suite with extended attributes', function() {
+        it('generates a suite marked as a "Component"', function() {
             var suite = FeatureStory.component('An application component', function() {});
+            
             expect(suite.description).toBe('Component: An application component');
             expect(suite.tags).toEqual(['component']);
         });
     });
     
     describe('scenario', function() {
-        it('should create a spec block within the current suite', function() {
+        it('creates a spec marked as a "Scenario" within the current suite', function() {
             var parentSuite = env.describe('dummy suite', function() {});
             jasmine.grammar.getEnv().currentSuite = parentSuite;
             
