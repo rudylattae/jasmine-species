994a9a835c29f98243b4f93941ba72069c0be3d1
rudylattae <rudylattae@gmail.com>
2010-12-22 00:37:33 -0600

    Edited the XDoc specs for clarity.

--- spec/jasmine-grammar/XDocSpecs.js
+++ spec/jasmine-grammar/XDocSpecs.js
@@ -8,42 +8,46 @@ describe('jasmine.grammar.XDoc', function() {
     });
     
     describe('topic', function() {
-        it('should create a suite with extended attributes', function() {
+        it('creates a suite tagged with "topic"', function() {
             var suite = XDoc.topic('An interesting topic', function() {});
+            
             expect(suite.description).toBe('An interesting topic');
             expect(suite.tags).toEqual(['topic']);
         });
     });
     
     describe('example', function() {
-        it('should create a suite with extended attributes', function() {
+        it('creates a suite tagged with "example"', function() {
             var suite = XDoc.example('A specific example', function() {});
+            
             expect(suite.description).toBe('A specific example');
             expect(suite.tags).toEqual(['example']);
         });
         
-        it('should create a suite whose spec definitions are exposed via the defs atribute', function() {
+        it('creates a suite whose spec definitions are exposed via the defs atribute', function() {
             var suite = XDoc.example('A specific example', function() {});
+            
             expect(suite.description).toBe('A specific example');
             expect(suite.expose).toBe(true);
             expect(suite.defs).not.toBe(null);
         });
         
-        it('should strip the "function() { ... }" block delimeters when it generates the def attribute', function() {
+        it('strips the "function() { ... }" block delimeters when it generates the def attribute', function() {
             var suite = XDoc.example('A specific example', function() {
                 env.it("uses only this block", function () {});
             });
+            
             expect(suite.defs).toBe('env.it("uses only this block", function () {});');
         });
     });
     
     describe('pass', function() {
-        it('should create an end result specification item', function() {
-            // we need a suite before a spec
+        it('creates a spec in the current suite', function() {
             var parentSuite = XDoc.example('A simple example', function() {});
             jasmine.grammar.getEnv().currentSuite = parentSuite;
             
             var spec = XDoc.pass('This should pass', function() {});
+            
             expect(spec.description).toBe('This should pass');
             expect(spec.suite).toBe(parentSuite);
         });
